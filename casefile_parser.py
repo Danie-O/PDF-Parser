395ef1294ca4471c347fe4bfa4d8acd060cc3d8f
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-03-15 19:32:16 +0000

    adding metadata schema and updates to parser

--- casefile_parser.py
+++ casefile_parser.py
@@ -1,17 +1,37 @@
-"""
-    A script for parsing pdf files of court cases and extracting data required to generate the following for each case file;
-    - a metadata.json file
-    - a preview.md file
-    - a technical.txt file
-    - an error log file
-    - an info log file
-"""
-
 import os
 import re
 import json
-import PyPDF2
+from PyPDF2 import PdfFileReader
+from pathlib import Path
+# import textract
+# import pdfminer
+import logging
 import datetime
+
+"""
+    A script for parsing pdf files of court cases and extracting data required to generate the following for each case file;
+    - a metadata.json file, a preview.md file, a technical.txt file, an error log file, an info log file
+"""
+# define paths for the following; 
+input_dir = "./input"
+output_dir = "./output"
+error_log = "./errors.log"
+info_log = "./errors.log"
+
+# set up loggers to handle errors and info about erroneous metadata files
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+error_logger = logging.getLogger('error')
+error_logger.setLevel(logging.ERROR)
+info_logger = logging.getLogger('info')
+info_logger.setLevel(logging.INFO)
+
+error_handler = logging.FileHandler('error.log')
+error_handler.setLevel(logging.ERROR)
+error_logger.addHandler(error_logger)
+
+info_handler = logging.FileHandler('info.log')
+info_handler.setLevel(logging.INFO)
+info_logger.addHandler(info_handler)
 
 # define helper functions to handle PDF parsing and output files generation
 def extract_metadata(pdf):
@@ -32,6 +52,25 @@ def generate_preview(x):
     """
     pass
 
+def generate_technical(file_path: str):
+    """
+    Generates a technical text file from the contents of the pdf file passed as input
+
+    Args:
+        file_path (str): the path to the pdf file being parsed
+    """
+    output_file = f""
+    pdf = PdfFileReader(file_path)
+    pages = pdf._get_num_pages()
+    
+    with Path(output_file).open(mode='w') as output:
+        text = ''
+        for page in pdf.pages:
+            text += page.extract_text()
+        output_file.write(text)
+    
+
+
 def generate_files(x):
     """
     Generates metadata, preview and technical files for a given pdf file
@@ -51,3 +90,4 @@ def parse_files(file):
     pass
 
 # define main function to generate all required files(including log files), for all pdf files in a given directory path
+
